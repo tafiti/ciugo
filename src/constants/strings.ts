5a28ef7f7f61aa97782ac1685f17dce8f780df70
tafiti <11234317+tafiti@users.noreply.github.com>
2022-03-19 01:24:57 -0400

    stings expression fix

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -4,7 +4,6 @@ export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Ndemwa tijiganu'
 export const WORD_NOT_FOUND_MESSAGE = 'Kiugo kĩu nĩ ũgeni'
 export const HARD_MODE_ALERT_MESSAGE = 'Nĩkũũma na ngũri (Hard Mode)'
-  'Hard Mode can only be enabled at the start!'
 export const HARD_MODE_DESCRIPTION =
   'Any revealed hints must be used in subsequent guesses'
 export const HIGH_CONTRAST_MODE_DESCRIPTION = 'Nĩguo woone ndemwa weega'
