9034cbbcd25e02891dfda19471251e550acbcaef
tafiti <11234317+tafiti@users.noreply.github.com>
2022-04-05 18:04:57 -0400

    edit strings.ts style

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -4,15 +4,11 @@ export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Ndemwa tijiganu'
 export const WORD_NOT_FOUND_MESSAGE = 'Kiugo kĩu nĩ kĩgeni'
 export const HARD_MODE_ALERT_MESSAGE = 'Nĩkũũma na ngũri (Hard Mode)'
-export const HARD_MODE_DESCRIPTION =
-  'Wonio mbũgũ no mũhaka ũmĩhũthĩre'
+export const HARD_MODE_DESCRIPTION = 'Wonio mbũgũ no mũhaka ũmĩhũthĩre'
 export const HIGH_CONTRAST_MODE_DESCRIPTION = 'Akia nĩguo woone ndemwa weega'
-export const CORRECT_WORD_MESSAGE = (solution: string) =>
-  `Kiugo kiuma ${solution}`
-export const WRONG_SPOT_MESSAGE = (guess: string, position: number) =>
-  `Must use ${guess} in position ${position}`
-export const NOT_CONTAINED_MESSAGE = (letter: string) =>
-  `Guess must contain ${letter}`
+export const CORRECT_WORD_MESSAGE = (solution: string) => `Kiugo kiuma ${solution}`
+export const WRONG_SPOT_MESSAGE = (guess: string, position: number) => `Must use ${guess} in position ${position}`
+export const NOT_CONTAINED_MESSAGE = (letter: string) => `Guess must contain ${letter}`
 export const ENTER_TEXT = 'Enter'
 export const DELETE_TEXT = 'Delete'
 export const STATISTICS_TITLE = 'Gũtarana'
